f840f9ccfbe0fddfbecd012b4c96b9ffe1fb1c76
dreyWesson <dreyWesson@gmail.com>
2019-12-23 17:53:01 +0100

    removed margin-bottom from p-tag

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service_worker.js')
-    .then((reg) => console.log(`service worker registered`, reg))
-    .catch((err) => console.log(`service worker not registered: ${err}`));
+  window.addEventListener('load', ()=>{
+    navigator.serviceWorker.register('service_worker.js')
+      .then((reg) => console.log(`service worker registered`, reg))
+      .catch((err) => console.log(`service worker not registered: ${err}`));    
+  })
 }
 
 
